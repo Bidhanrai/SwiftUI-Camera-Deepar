0978b00f860d4a74c3455af2a49813316d1fb672
Bidhan Rai <bidhanrai424@gmail.com>
2022-06-08 16:01:58 +0545

    first commit

---
+++ Fun Camera/Fun_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_CameraApp.swift
+//  Fun Camera
+//
+//  Created by Bidhan Rai on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fun_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fun CameraUITests/Fun_CameraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fun_CameraUITestsLaunchTests.swift
+//  Fun CameraUITests
+//
+//  Created by Bidhan Rai on 08/06/2022.
+//
+
+import XCTest
+
+class Fun_CameraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
